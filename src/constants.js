45acd4c2f3125cf21c03530b672cbb9236103a9e
David <davidabutler92@gmail.com>
2021-03-25 11:11:18 -0700

    add types array constant

---
+++ src/constants.js
@@ -0,0 +1,92 @@
+// const types = [
+//   "bug",
+//   "fairy",
+//   "fire",
+//   "ghost",
+//   "ground",
+//   "normal",
+//   "psychic",
+//   "steel",
+//   "dark",
+//   "electric",
+//   "fighting",
+//   "flying",
+//   "grass",
+//   "ice",
+//   "poison",
+//   "rock",
+//   "water",
+// ];
+
+const types = [
+  {
+    title: "Bug",
+    name: "bug",
+  },
+  {
+    title: "Fairy",
+    name: "fairy",
+  },
+  {
+    title: "Fire",
+    name: "fire",
+  },
+  {
+    title: "Ghost",
+    name: "ghost",
+  },
+  {
+    title: "Ground",
+    name: "ground",
+  },
+  {
+    title: "Normal",
+    name: "normal",
+  },
+  {
+    title: "Psychic",
+    name: "psychic",
+  },
+  {
+    title: "Steel",
+    name: "steel",
+  },
+  {
+    title: "Dark",
+    name: "Dark",
+  },
+  {
+    title: "Electric",
+    name: "electric",
+  },
+  {
+    title: "Fighting",
+    name: "fighting",
+  },
+  {
+    title: "Flying",
+    name: "flying",
+  },
+  {
+    title: "Grass",
+    name: "grass",
+  },
+  {
+    title: "Ice",
+    name: "ice",
+  },
+  {
+    title: "Poison",
+    name: "poison",
+  },
+  {
+    title: "Rock",
+    name: "rock",
+  },
+  {
+    title: "Water",
+    name: "water",
+  },
+];
+
+export default types;
