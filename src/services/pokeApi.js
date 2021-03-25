99bc0d41f6833285139533e8b69d1c53c23c9595
David <davidabutler92@gmail.com>
2021-03-25 11:12:54 -0700

    add getPokemonByType to filter by type

--- src/services/pokeApi.js
+++ src/services/pokeApi.js
@@ -4,13 +4,14 @@ export const getPokemon = (offset) => {
   return axios
     .get(`https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=20`)
     .then((res) => {
-      if (res.status !== 200) throw new Error("could not get pokemon");
+      if (res.status !== 200)
+        throw new Error("No Pokémon matched your search.");
 
       return Promise.all(
         res.data.results.map((pokemon) => {
           return axios.get(pokemon.url).then((res) => {
             if (res.status !== 200)
-              throw new Error("could not get this pokemon");
+              throw new Error("No Pokémon matched your search.");
             return res;
           });
         })
@@ -29,8 +30,39 @@ export const getPokemonByName = (name) => {
       },
     })
     .then((res) => {
-      if (res.status !== 200) throw new Error(`could not get ${name}`);
+      if (res.status !== 200)
+        throw new Error("No Pokémon matched your search.");
       return res;
+    })
+    .catch((err) => {
+      return err;
+    });
+};
+
+export const getPokemonByType = (typeArray) => {
+  return Promise.all(
+    typeArray.map((type) => {
+      return axios.get(`https://pokeapi.co/api/v2/type/${type}`, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    })
+  )
+    .then((res) => {
+      console.log(res);
+      const CombinedArray = res
+        .map((item) => {
+          return item.data.pokemon;
+        })
+        .flat();
+
+      console.log(CombinedArray[0].pokemon.name, "caterpie");
+
+      const uniquePokemonNames = [
+        ...new Set(CombinedArray.map((data) => data.pokemon.name)),
+      ].map((name) => CombinedArray.find((data) => console.log(data)));
+      console.log(uniquePokemonNames);
     })
     .catch((err) => {
       return err;
